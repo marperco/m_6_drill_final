7327258ad11e7d35a6e962d2eb8ad35ffae8c5fe
Marcelo Perez <marperco@gmail.com>
2023-08-29 23:38:02 -0400

    Commit definitivo, falta ultima parte

---
+++ vehiculo/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Vehiculo(models.Model):
+    MARCA_CHOICES = [
+        ('Ford', 'Ford'),
+        ('Chevrolet', 'Chevrolet'),
+        ('Fiat', 'Fiat'),
+        ('Toyota', 'Toyota'),
+    ]
+
+    CATEGORIA_CHOICES = [
+        ('Particular', 'Particular'),
+        ('Transporte', 'Transporte'),
+        ('Carga', 'Carga'),
+    ]
+
+    marca = models.CharField(max_length=20, choices=MARCA_CHOICES, default='Ford')
+    modelo = models.CharField(max_length=100)
+    serial_carroceria = models.CharField(max_length=50)
+    serial_motor = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=20, choices=CATEGORIA_CHOICES, default='Particular')
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo}"
+
